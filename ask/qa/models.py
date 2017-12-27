6573514f0cf0260f824f1a95fe5d0437187be935
0rps <tuxrps@gmail.com>
2017-12-27 23:14:51 +0300

    [04 lab] models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Question(models.Model):
+
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+
+class QuestionManager:
+
+    @staticmethod
+    def new(self):
+        return Question.objects.latest('added_at')
+
+    @staticmethod
+    def popular(self):
+        return Question.objects.order_by('-rating')
