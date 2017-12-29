7d76b3b7324a23166b15e345e0fc914bfbdd71be
0rps <tuxrps@gmail.com>
2017-12-29 22:25:12 +0300

    [04]

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,30 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.latest('added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 # Create your models here.
 class Question(models.Model):
 
+    objects = QuestionManager()
+
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
     likes = models.ManyToManyField(User)
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(blank=True, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
 
 
-class QuestionManager:
 
-    @staticmethod
-    def new(self):
-        return Question.objects.latest('added_at')
-
-    @staticmethod
-    def popular(self):
-        return Question.objects.order_by('-rating')
